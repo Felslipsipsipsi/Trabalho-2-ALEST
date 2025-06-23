953b6508491c97d39fa1890cf7645bec2d4e3b1f
Felslipsipsipsi <felslipe.alvesbrandao@gmail.com>
2025-06-22 22:45:37 -0300

    Update Arquivo.java

--- Arquivo.java
+++ Arquivo.java
@@ -1,31 +1,44 @@
 package arvorebinariapesquisa;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner;
 
 public class Arquivo {
+
     public static String[] ler(String nome) {
-        ArrayList<String> linhas = new ArrayList<>();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(nome));
-            String linha;
-            while ((linha = br.readLine()) != null) {
-                linhas.add(linha);
+        String[] linhas = new String[100];
+        int i = 0;
+
+        Scanner sc = null;
+        File arquivo = new File(nome);
+        if (arquivo.exists()) {
+            try {
+                sc = new Scanner(arquivo);
+                while (sc.hasNextLine() && i < linhas.length) {
+                    linhas[i] = sc.nextLine();
+                    i++;
+                }
+                sc.close();
+            } catch (Exception e) {
+                System.out.println("Erro na leitura.");
             }
-            br.close();
-        } catch (Exception e) {
-            System.out.println("Erro ao ler o arquivo: " + nome);
+        } else {
+            System.out.println("Arquivo não encontrado.");
         }
-        return linhas.toArray(new String[0]);
+
+        return linhas;
     }
 
     public static void gravar(String nome, String conteudo) {
+        java.io.File f = new java.io.File(nome);
+        java.io.PrintStream ps = null;
         try {
-            PrintWriter pw = new PrintWriter(nome);
-            pw.println(conteudo);
-            pw.close();
+            ps = new java.io.PrintStream(f);
+            ps.println(conteudo);
+            ps.close();
         } catch (Exception e) {
-            System.out.println("Erro ao gravar o arquivo: " + nome);
+            System.out.println("Erro ao gravar.");
         }
     }
 }
+
